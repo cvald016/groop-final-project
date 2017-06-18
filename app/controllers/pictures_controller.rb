965ae85ee1070df056fa786384ddcd2e9d9e79b7
Christopher <cvald016@fiu.edu>
2017-06-18 00:05:22 -0400

    fix pic controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,13 +1,31 @@
 class PicturesController < ApplicationController
   def index
-  end
+      @pictures = Picture.all
+   end
 
-  def new
-  end
+   def new
+      @picture = Picture.new
+   end
 
-  def create
-  end
+   def create
+      @picture = Picture.new(picture_params)
 
-  def destroy
-  end
+      if @picture.save
+         redirect_to picture_path, notice: "The resume #{@picture.name} has been uploaded."
+      else
+         render "new"
+      end
+
+   end
+
+   def destroy
+      @picture = Picture.find(params[:id])
+      @picture.destroy
+      redirect_to picture_path, notice:  "The resume #{@picture.name} has been deleted."
+   end
+
+   private
+      def picture_params
+      params.require(:picture).permit(:name, :attachment)
+   end
 end
