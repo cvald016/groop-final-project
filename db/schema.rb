5d5e712c09887764016e886d32f6d152777bb088
Christopher <cvald016@fiu.edu>
2017-06-17 23:57:36 -0400

    adds picture model/controller/uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620212727) do
+
+ActiveRecord::Schema.define(version: 20170618035547) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -36,6 +37,13 @@ ActiveRecord::Schema.define(version: 20170620212727) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_photos_on_event_id"
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "name"
+    t.string "attachment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user_events", force: :cascade do |t|
