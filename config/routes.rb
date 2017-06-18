67c3052bcce1719402f5809c73d81ab4279330c7
Christopher <cvald016@fiu.edu>
2017-06-18 00:02:38 -0400

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,21 @@
 Rails.application.routes.draw do
-  get 'pictures/index'
-
-  get 'pictures/new'
-
-  get 'pictures/create'
-
-  get 'pictures/destroy'
-
-  # get 'auth/:provider/callback', to: 'sessions#create'
-  # get 'auth/failure', to: redirect('/')
-  # get 'signout', to: 'sessions#destroy', as: 'signout'
-
-  get 'auth/facebook', as: "login"
-  get 'auth/facebook/callback', to: 'sessions#create'
-  get 'logout'   => 'sessions#destroy', as: "logout"
-
-
-  resources :sessions, only: [:create, :destroy]	
-
-
-
-  root 'home#index'
-
+  resources :sessions, only: [:create, :destroy]
+  resources :pictures, only: [:index, :new, :create, :destroy]
   resources :photos
   resources :user_events
   resources :events
   resources :users
+    root "pictures#index"
+    #root 'home#index'
+      get 'pictures/index'
+      get 'pictures/new'
+      get 'pictures/create'
+      get 'pictures/destroy'
+      # get 'auth/:provider/callback', to: 'sessions#create'
+      # get 'auth/failure', to: redirect('/')
+      # get 'signout', to: 'sessions#destroy', as: 'signout'
+      get 'auth/facebook', as: "login"
+      get 'auth/facebook/callback', to: 'sessions#create'
+      get 'logout'   => 'sessions#destroy', as: "logout"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
